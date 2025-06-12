4bd9ee1ec05a2dc9da70d91b57525e78582aa54f
André Vinicius <andrevinicius.dev@gmail.com>
2025-06-11 23:22:07 -0300

    ajuste nos imports

--- src/components/carrossel/CarrosselIMG.vue
+++ src/components/carrossel/CarrosselIMG.vue
@@ -1,75 +1,92 @@
 <script setup>
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 import { ref } from 'vue';
+import img1 from '@assets/img/img1.jpeg';
+import img2 from '@assets/img/img2.jpeg';
+import img3 from '@assets/img/img3.jpeg';
+// import img4 from '@assets/img/img4.jpeg';
+import img5 from '@assets/img/img5.jpeg';
+import img6 from '@assets/img/img6.jpeg';
+import img7 from '@assets/img/img7.jpeg';
+import img8 from '@assets/img/img8.jpeg';
+import img9 from '@assets/img/img9.jpeg';
+import img10 from '@assets/img/img10.jpeg';
+import img11 from '@assets/img/img11.jpeg';
+import img12 from '@assets/img/img12.jpeg';
+import img13 from '@assets/img/img13.jpeg';
+import img14 from '@assets/img/img14.jpeg';
+import img15 from '@assets/img/img15.jpeg';
+import img16 from '@assets/img/img16.jpeg';
+import img17 from '@assets/img/img17.jpeg';
 
 const carrossel = ref(null);
 const images = [{
   'id': 1,
-  'url': '/src/assets/img/img1.jpeg'
+  'url': img1
 },
 {
   'id': 2,
-  'url': '/src/assets/img/img2.jpeg'
+  'url': img2
 },
 {
   'id': 3,
-  'url': '/src/assets/img/img3.jpeg'
+  'url': img3
 },
 // {
 //   'id': 4,
-//   'url': '/src/assets/img/img4.jpeg'
+//   'url': img4
 // },
 {
   'id': 5,
-  'url': '/src/assets/img/img5.jpeg'
+  'url': img5
 },
 {
   'id': 6,
-  'url': '/src/assets/img/img6.jpeg'
+  'url': img6
 },
 {
   'id': 7,
-  'url': '/src/assets/img/img7.jpeg'
+  'url': img7
 },
 {
   'id': 8,
-  'url': '/src/assets/img/img8.jpeg'
+  'url': img8
 },
 {
   'id': 9,
-  'url': '/src/assets/img/img9.jpeg'
+  'url': img9
 },
 {
   'id': 10,
-  'url': '/src/assets/img/img10.jpeg'
+  'url': img10
 },
 {
   'id': 11,
-  'url': '/src/assets/img/img11.jpeg'
+  'url': img11
 },
 {
   'id': 12,
-  'url': '/src/assets/img/img12.jpeg'
+  'url': img12
 },
 {
   'id': 13,
-  'url': '/src/assets/img/img13.jpeg'
+  'url': img13
 },
 {
   'id': 14,
-  'url': '/src/assets/img/img14.jpeg'
+  'url': img14
 },
 {
   'id': 15,
-  'url': '/src/assets/img/img15.jpeg'
+  'url': img15
 },
 {
   'id': 16,
-  'url': '/src/assets/img/img17.jpeg'
+  'url': img16
 },
 {
   'id': 17,
-  'url': '/src/assets/img/img17.jpeg'
+  'url': img17
 }
 ];
 
